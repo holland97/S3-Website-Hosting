c6dd80af35913aa57efab8b5e1dfb6f8b12522ce
Roderick <theprophetelijah97@gmail.com>
2024-08-30 16:31:36 -0400

    added data source

--- website-config/vars.tf
+++ website-config/vars.tf
@@ -6,13 +6,17 @@ variable "static-bkt" {
 
 variable "region" {
   description = "Region of AWS account"
-  type = string
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "certificate-arn" {
   description = "ARN of my certificate"
-  type = string
-  default = "arn:aws:acm:us-east-1:796973480898:certificate/b72aa12a-5c7b-4657-aa5e-421b77c1f3c9"
-  
+  type        = string
+  default     = "arn:aws:acm:us-east-1:796973480898:certificate/b72aa12a-5c7b-4657-aa5e-421b77c1f3c9"
+
+}
+
+data "aws_route53_zone" "my-zone" {
+  name = "rodscloudresume.org"
 }
