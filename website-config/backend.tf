9816373f8da08c92b186188c35c85cd5cda6706d
Roderick <theprophetelijah97@gmail.com>
2024-08-30 16:32:53 -0400

    added backend back

--- website-config/backend.tf
+++ website-config/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "rodscloudresume-state-bkt9999"
     dynamodb_table = "rodscloudresume-db-table9999"
-    key = "cloudresume/terraform.tfstate"
+    key            = "cloudresume/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
 
